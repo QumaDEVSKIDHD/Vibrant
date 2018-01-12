1f2bac833f445a2bdfcb2cf41c24523733890276
Cydhra <j-hengstler@web.de>
2018-01-12 15:28:05 +0100

    disable flight when module is disable

--- vibrant-client/src/main/kotlin/net/cydhra/vibrant/modules/movement/FlyModule.kt
+++ vibrant-client/src/main/kotlin/net/cydhra/vibrant/modules/movement/FlyModule.kt
@@ -7,19 +7,22 @@ import net.cydhra.vibrant.VibrantClient
 import net.cydhra.vibrant.events.minecraft.MinecraftTickEvent
 import net.cydhra.vibrant.modulesystem.DefaultCategories
 import net.cydhra.vibrant.modulesystem.Module
+import org.lwjgl.input.Keyboard
 
 /**
  *
  */
-class FlyModule : Module("Fly", DefaultCategories.MOVEMENT) {
+class FlyModule : Module("Fly", DefaultCategories.MOVEMENT, Keyboard.KEY_F) {
+
+    override fun onDisable() {
+        VibrantClient.minecraft.thePlayer?.isFlying = false
+    }
 
     @EventHandler
     fun onTick(e: MinecraftTickEvent) {
-        if (VibrantClient.minecraft.thePlayer != null) {
-            if (!VibrantClient.minecraft.thePlayer!!.onGround) {
-                VibrantClient.minecraft.thePlayer!!.isAllowedFlying = true
-                VibrantClient.minecraft.thePlayer!!.isFlying = true
-            }
+        if (VibrantClient.minecraft.thePlayer?.onGround == false) {
+            VibrantClient.minecraft.thePlayer?.isAllowedFlying = true
+            VibrantClient.minecraft.thePlayer?.isFlying = true
         }
     }
 }
