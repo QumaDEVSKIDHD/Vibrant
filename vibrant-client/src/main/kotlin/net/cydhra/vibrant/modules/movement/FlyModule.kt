6d468233e565ac31b7afabd7d52c8ee96168e9a4
Cydhra <j-hengstler@web.de>
2018-01-30 20:10:14 +0100

    Fly now uses FlightResource

--- vibrant-client/src/main/kotlin/net/cydhra/vibrant/modules/movement/FlyModule.kt
+++ vibrant-client/src/main/kotlin/net/cydhra/vibrant/modules/movement/FlyModule.kt
@@ -2,11 +2,13 @@
 
 package net.cydhra.vibrant.modules.movement
 
-import net.cydhra.eventsystem.listeners.EventHandler
 import net.cydhra.vibrant.VibrantClient
-import net.cydhra.vibrant.events.minecraft.MinecraftTickEvent
 import net.cydhra.vibrant.modulesystem.DefaultCategories
 import net.cydhra.vibrant.modulesystem.Module
+import net.cydhra.vibrant.organization.GameResourceManager
+import net.cydhra.vibrant.organization.channel.ResourceChannel
+import net.cydhra.vibrant.organization.priorities.ResourceRequestPriority
+import net.cydhra.vibrant.organization.resources.FlyingResource
 import org.lwjgl.input.Keyboard
 
 /**
@@ -15,16 +17,20 @@ import org.lwjgl.input.Keyboard
 class FlyModule : Module("Fly", DefaultCategories.MOVEMENT, Keyboard.KEY_F) {
 
     override fun onDisable() {
-        VibrantClient.minecraft.thePlayer?.isFlying = false
+        // this should not be done, but it is the simplest way
+        mc.thePlayer?.isFlying = false
     }
 
-    @EventHandler
-    fun onTick(e: MinecraftTickEvent) {
+    override fun doRequestResources() {
         if (VibrantClient.minecraft.thePlayer != null) {
-            val grounded = !VibrantClient.minecraft.thePlayer!!.onGround
-
-            VibrantClient.minecraft.thePlayer!!.isAllowedFlying = grounded
-            VibrantClient.minecraft.thePlayer!!.isFlying = grounded
+            if (!VibrantClient.minecraft.thePlayer!!.onGround) {
+                GameResourceManager.requestGameResource(
+                        FlyingResource,
+                        FlyingResource.FlyingResourceState(true, true),
+                        ResourceRequestPriority.movement,
+                        ResourceChannel.Side.CLIENT
+                )
+            }
         }
     }
 }
