d02eae89538ea26b15bf462dd1a5286a80d09bf5
Lukas <flaflo@ok.de>
2018-01-31 19:09:39 +0100

    Added default Keybind G

--- vibrant-client/src/main/kotlin/net/cydhra/vibrant/modules/movement/SprintModule.kt
+++ vibrant-client/src/main/kotlin/net/cydhra/vibrant/modules/movement/SprintModule.kt
@@ -5,11 +5,12 @@ import net.cydhra.vibrant.modulesystem.Module
 import net.cydhra.vibrant.organization.GameResourceManager
 import net.cydhra.vibrant.organization.priorities.ResourceRequestPriority
 import net.cydhra.vibrant.organization.resources.SprintingResource
+import org.lwjgl.input.Keyboard
 
 /**
  *
  */
-class SprintModule : Module("Sprint", DefaultCategories.MOVEMENT) {
+class SprintModule : Module("Sprint", DefaultCategories.MOVEMENT, Keyboard.KEY_G) {
 
     override fun doRequestResources() {
         if (mc.thePlayer?.isCollidedHorizontally == false && mc.thePlayer!!.movementInput.moveForward > 0 && mc.thePlayer!!.onGround) {
