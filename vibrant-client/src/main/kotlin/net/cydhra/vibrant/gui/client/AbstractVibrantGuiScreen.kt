5175202c36b67fd4795091b88af06ccc7e444266
Cydhra <j-hengstler@web.de>
2018-02-05 17:18:39 +0100

    implemented drawDefaultBackground for AbstractVibrantGuiScreen

--- vibrant-client/src/main/kotlin/net/cydhra/vibrant/gui/client/AbstractVibrantGuiScreen.kt
+++ vibrant-client/src/main/kotlin/net/cydhra/vibrant/gui/client/AbstractVibrantGuiScreen.kt
@@ -1,15 +1,18 @@
 package net.cydhra.vibrant.gui.client
 
 import net.cydhra.vibrant.VibrantClient
+import net.cydhra.vibrant.api.client.VibrantMinecraft
+import net.cydhra.vibrant.api.gui.VibrantGuiController
 import net.cydhra.vibrant.api.gui.VibrantGuiScreen
 import net.cydhra.vibrant.gui.screen.VibrantScreen
 import net.cydhra.vibrant.gui.util.GlStateManager
 import org.lwjgl.input.Keyboard
+import org.lwjgl.opengl.GL11
 
 /**
  * This class extends [VibrantGuiScreen] to override all the properties that must be implemented but are initialized by the adapter class.
  */
-abstract class AbstractVibrantGuiScreen : VibrantGuiScreen {
+abstract class AbstractVibrantGuiScreen(private val controller: VibrantGuiController) : VibrantGuiScreen {
 
     protected open val clickGuiScreen = VibrantScreen()
 
@@ -19,8 +22,20 @@ abstract class AbstractVibrantGuiScreen : VibrantGuiScreen {
     private var isDragging = false
     protected var isInitialized = false
 
+    protected val mc: VibrantMinecraft
+        get() = VibrantClient.minecraft
+
+    private var wallpapers: Array<String> = arrayOf(
+            "textures/wp1.jpg",
+            "textures/wp2.jpg",
+            "textures/wp3.png",
+            "textures/wp4.jpg")
+
+    constructor() : this(VibrantClient.factory.newGuiController())
+
+
     override fun initGui() {
-        if(!isInitialized) {
+        if (!isInitialized) {
             this.initGuiFirstTime()
             isInitialized = true
         }
@@ -64,6 +79,28 @@ abstract class AbstractVibrantGuiScreen : VibrantGuiScreen {
         if (isDragging) {
             clickGuiScreen.dragReleased(mouseX, mouseY, state)
             isDragging = false
+        }
+    }
+
+    fun drawDefaultBackground() {
+        if (mc.theWorld != null) {
+
+        } else {
+            // render a wallpaper of the array and switch it every minute (60000 ms)
+            mc.getTextureManager().bindTexture(wallpapers[(System.currentTimeMillis() / 60000 % wallpapers.size).toInt()])
+            controller.drawRectWithCustomSizedTexture(0, 0, 0f, 0f, this.width, this.height, this.width.toFloat(), this.height.toFloat())
+
+            // if 55 seconds of the minute are over, blend the next image smoothly above the current
+            if (System.currentTimeMillis() / 1000 % 60 > 54) {
+                mc.getTextureManager().bindTexture(wallpapers[((System.currentTimeMillis() / 60000 + 1) % wallpapers.size).toInt()])
+                GL11.glEnable(GL11.GL_BLEND)
+//                GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA)
+
+                // alpha value depends on current millisecond
+                GL11.glColor4f(1f, 1f, 1f, 1 - (60000 - System.currentTimeMillis() % 60000) / 5000f)
+                controller.drawRectWithCustomSizedTexture(0, 0, 0f, 0f, this.width, this.height, this.width.toFloat(), this.height.toFloat())
+                GL11.glDisable(GL11.GL_BLEND)
+            }
         }
     }
 }
