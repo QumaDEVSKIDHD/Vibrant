9af52d3c78dd51e96875fad1665d7431c794de47
Cydhra <j-hengstler@web.de>
2018-02-10 20:12:40 +0100

    hooked RenderManager.isDebugBoundingBox

--- vibrant-interface/src/main/kotlin/net/cydhra/vibrant/api/render/VibrantRenderManager.kt
+++ vibrant-interface/src/main/kotlin/net/cydhra/vibrant/api/render/VibrantRenderManager.kt
@@ -7,6 +7,8 @@ interface VibrantRenderManager {
     var renderPosY: Double
     var renderPosZ: Double
 
+    var isDebugBoundingBox: Boolean
+
     fun renderEntitySimple(entity: VibrantEntity, partialTicks: Float, unused: Any?): Boolean
 
     fun <T : VibrantEntity> getEntityRenderObj(entity: T): VibrantRender<T>
