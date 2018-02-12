d5bc3604a905a7c54389d2d053ea94286abb1d39
Cydhra <j-hengstler@web.de>
2018-02-12 01:34:45 +0100

    implemented empty gradle task for workspace setup

---
+++ buildSrc/src/main/groovy/net/cydhra/vibrant/tasks/SetupWorkspace.groovy
@@ -0,0 +1,12 @@
+package net.cydhra.vibrant.tasks
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.TaskAction
+
+class SetupWorkspace extends DefaultTask {
+
+    @TaskAction
+    def setupWorkspace() {
+
+    }
+}
