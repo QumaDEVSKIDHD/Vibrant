e370bd7295309a855b5b6e5f04a10c41d5a2c80a
Cydhra <j-hengstler@web.de>
2018-03-07 19:23:49 +0100

    temp removed SmartMover3D again and SmartMover2D is now AngleSmartMover

--- vibrant-client/src/main/kotlin/net/cydhra/vibrant/util/math/SmartMover.kt
+++ vibrant-client/src/main/kotlin/net/cydhra/vibrant/util/math/SmartMover.kt
@@ -3,16 +3,16 @@ package net.cydhra.vibrant.util.math
 /**
  *
  */
-class SmartMover2D(x: Double, y: Double) {
-    private var components: Pair<Double, Double> = Pair(x, y)
+class AngleSmartMover(initialConfiguration: Angle) {
+    private var components: Pair<Double, Double> = Pair(initialConfiguration.yaw, initialConfiguration.pitch)
     private var prevComponentVector: Pair<Double, Double> = components
 
     /**
      * Update the current vector state and start interpolating with the previously set state.
      */
-    fun update(x: Double, y: Double) {
+    fun update(newConfiguration: Angle) {
         prevComponentVector = this.components
-        this.components = Pair(x, y)
+        this.components = Pair(newConfiguration.yaw, newConfiguration.pitch)
     }
 
     /**
@@ -32,40 +32,5 @@ class SmartMover2D(x: Double, y: Double) {
         val (prevX, prevY) = prevComponentVector
         val (currX, currY) = components
         return Pair(prevX + (currX - prevX) * partialTicks, prevY + (currY - prevY) * partialTicks)
-    }
-}
-
-class SmartMover3D(x: Double, y: Double, z: Double) {
-    private var components: Triple<Double, Double, Double> = Triple(x, y, z)
-    private var prevComponentVector: Triple<Double, Double, Double> = components
-
-    /**
-     * Update the current vector state and start interpolating with the previously set state.
-     */
-    fun update(x: Double, y: Double, z: Double) {
-        prevComponentVector = this.components
-        this.components = Triple(x, y, z)
-    }
-
-    /**
-     * The state used for interpolation is reset to the latest known state (so interpolation will always return the latest state until
-     * [update] is called again)
-     */
-    fun reset() {
-        this.prevComponentVector = components
-    }
-
-    /**
-     * Interpolate between the current and the previous state using the [partialTicks] as the time scale
-     *
-     * @param partialTicks a linear value between 0 and 1 indicating the time until a game tick has fully passed
-     */
-    fun interpolate(partialTicks: Float): Triple<Double, Double, Double> {
-        val (prevX, prevY, prevZ) = prevComponentVector
-        val (currX, currY, currZ) = components
-        return Triple(
-                prevX + (currX - prevX) * partialTicks,
-                prevY + (currY - prevY) * partialTicks,
-                prevZ + (currZ - prevZ) * partialTicks)
     }
 }
