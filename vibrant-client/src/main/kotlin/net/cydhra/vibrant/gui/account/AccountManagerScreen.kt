852b6ac54003f781d7c82ca4850cfa4e6227d8fa
Cydhra <j-hengstler@web.de>
2018-03-13 22:28:40 +0100

    replaced account manager password textbox with passwordbox

--- vibrant-client/src/main/kotlin/net/cydhra/vibrant/gui/account/AccountManagerScreen.kt
+++ vibrant-client/src/main/kotlin/net/cydhra/vibrant/gui/account/AccountManagerScreen.kt
@@ -6,6 +6,7 @@ import net.cydhra.vibrant.VibrantClient
 import net.cydhra.vibrant.gui.client.AbstractVibrantGuiScreen
 import net.cydhra.vibrant.gui.component.impl.VibrantButton
 import net.cydhra.vibrant.gui.component.impl.VibrantListbox
+import net.cydhra.vibrant.gui.component.impl.VibrantPasswordbox
 import net.cydhra.vibrant.gui.component.impl.VibrantTextbox
 
 /**
@@ -28,7 +29,7 @@ class AccountManagerScreen(private val parent: AbstractVibrantGuiScreen) : Abstr
 
         accountListbox = VibrantListbox(20.0, 20.0, this.width - 40.0, this.height - 100.0, "Accounts")
         usernameTextbox = VibrantTextbox(this.width / 2 - 205.0, this.height - 60.0, 200.0, 20.0, "username")
-        passwordTextbox = VibrantTextbox(this.width / 2 + 5.0, this.height - 60.0, 200.0, 20.0, "password")
+        passwordTextbox = VibrantPasswordbox(this.width / 2 + 5.0, this.height - 60.0, 200.0, 20.0, "password")
 
         this.clickGuiScreen.addComponent(accountListbox)
         this.clickGuiScreen.addComponent(usernameTextbox)
