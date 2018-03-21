37eb09e7ef2adbd982ddfbb9d5982e621a9647af
Cydhra <ubezl@student.kit.edu>
2018-03-21 16:35:28 +0100

    also added mcp options to unix command line

--- buildSrc/src/main/groovy/net/cydhra/vibrant/tasks/SetupWorkspace.groovy
+++ buildSrc/src/main/groovy/net/cydhra/vibrant/tasks/SetupWorkspace.groovy
@@ -51,7 +51,7 @@ class SetupWorkspace extends DefaultTask {
                 osfamily: "windows"
         )
         ant.exec(
-                command: "/bin/bash decompile.sh",
+                command: "/bin/bash decompile.sh --client --norecompile",
                 dir: "tmp",
                 osfamily: "unix"
         )
