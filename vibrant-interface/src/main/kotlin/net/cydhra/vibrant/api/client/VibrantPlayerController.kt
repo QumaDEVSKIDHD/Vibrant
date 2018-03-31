f18808f2579796105ae19ed0913e60ef693d1a25
Cydhra <ubezl@student.kit.edu>
2018-03-31 23:12:28 +0200

    netHandler is now accessible through player controller

--- vibrant-interface/src/main/kotlin/net/cydhra/vibrant/api/client/VibrantPlayerController.kt
+++ vibrant-interface/src/main/kotlin/net/cydhra/vibrant/api/client/VibrantPlayerController.kt
@@ -1,8 +1,11 @@
 package net.cydhra.vibrant.api.client
 
 import net.cydhra.vibrant.api.entity.VibrantEntity
+import net.cydhra.vibrant.api.network.VibrantNetHandler
 
 interface VibrantPlayerController {
+
+    val netHandler: VibrantNetHandler
 
     fun attackEntity(entity: VibrantEntity)
 
