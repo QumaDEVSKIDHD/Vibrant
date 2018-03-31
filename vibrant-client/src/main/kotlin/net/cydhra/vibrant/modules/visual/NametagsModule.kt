18ab804b2433b8011d3260da0373ffc148fe18be
Lukas <flaflo@ok.de>
2018-04-01 00:55:49 +0200

    * Nametag is no longer drawn for the own player * Now using the new project method

--- vibrant-client/src/main/kotlin/net/cydhra/vibrant/modules/visual/NametagsModule.kt
+++ vibrant-client/src/main/kotlin/net/cydhra/vibrant/modules/visual/NametagsModule.kt
@@ -4,6 +4,7 @@ import net.cydhra.eventsystem.listeners.EventHandler
 import net.cydhra.vibrant.VibrantClient
 import net.cydhra.vibrant.api.entity.VibrantEntity
 import net.cydhra.vibrant.api.entity.VibrantEntityLiving
+import net.cydhra.vibrant.api.entity.VibrantPlayerSP
 import net.cydhra.vibrant.events.render.RenderOverlayEvent
 import net.cydhra.vibrant.events.render.RenderWorldEvent
 import net.cydhra.vibrant.gui.font.VibrantFontRenderer
@@ -11,33 +12,26 @@ import net.cydhra.vibrant.gui.util.RenderUtil
 import net.cydhra.vibrant.modulesystem.DefaultCategories
 import net.cydhra.vibrant.modulesystem.Module
 import org.lwjgl.input.Keyboard
-import org.lwjgl.opengl.Display
 import org.lwjgl.util.vector.Vector3f
 import java.awt.Font
 import java.util.*
 
 class NametagsModule : Module("Nametags", DefaultCategories.VISUAL, Keyboard.KEY_J) {
-    private val font = Font("Arial", Font.BOLD, 18)
-    private val nametagFontRenderer = VibrantFontRenderer(font)
+    private val fontRenderer = VibrantFontRenderer(Font("Arial", Font.BOLD, 18))
 
     private val frustum = VibrantClient.factory.newFrustum()
     private val nametags = LinkedList<Pair<VibrantEntity, Vector3f>>()
 
     @EventHandler
     fun onRenderOverlay(e: RenderOverlayEvent) {
-        for (nametag in nametags) {
-            val x = nametag.second.x / e.scaledResolution.getScaleFactor()
-            val y = (Display.getHeight() - nametag.second.y) / e.scaledResolution.getScaleFactor()
-
-            nametagFontRenderer.drawString(nametag.first.getName(), x - nametagFontRenderer.getStringWidth(nametag.first.getName()) / 2, y, -1)
-        }
+        nametags.forEach { fontRenderer.drawStringWithShadow(it.first.getName(), it.second.x - fontRenderer.getStringWidth(it.first.getName()) / 2, it.second.y, -1) }
     }
 
     @EventHandler
     fun onRenderWorld(e: RenderWorldEvent) {
         nametags.clear()
 
-        for (en: VibrantEntityLiving in mc.theWorld!!.getEntityList().filterIsInstance<VibrantEntityLiving>()) {
+        for (en: VibrantEntityLiving in mc.theWorld!!.getEntityList().filterIsInstance<VibrantEntityLiving>().filterNot { it is VibrantPlayerSP }) {
             val px = RenderUtil.interpolate(mc.thePlayer!!.posX, mc.thePlayer!!.prevPosX, mc.timer.renderPartialTicks)
             val py = RenderUtil.interpolate(mc.thePlayer!!.posY, mc.thePlayer!!.prevPosY, mc.timer.renderPartialTicks)
             val pz = RenderUtil.interpolate(mc.thePlayer!!.posZ, mc.thePlayer!!.prevPosZ, mc.timer.renderPartialTicks)
@@ -45,11 +39,11 @@ class NametagsModule : Module("Nametags", DefaultCategories.VISUAL, Keyboard.KEY
             frustum.setPosition(px, py, pz)
 
             if (frustum.isBoundingBoxInsideFrustum(en.boundingBox)) {
-                val x = RenderUtil.interpolate(en.posX, en.prevPosX, mc.timer.renderPartialTicks) - mc.getRenderManager().renderPosX
-                val y = RenderUtil.interpolate(en.posY, en.prevPosY, mc.timer.renderPartialTicks) - mc.getRenderManager().renderPosY
-                val z = RenderUtil.interpolate(en.posZ, en.prevPosZ, mc.timer.renderPartialTicks) - mc.getRenderManager().renderPosZ
+                val x = (RenderUtil.interpolate(en.posX, en.prevPosX, mc.timer.renderPartialTicks) - mc.getRenderManager().renderPosX).toFloat()
+                val y = (RenderUtil.interpolate(en.posY, en.prevPosY, mc.timer.renderPartialTicks) - mc.getRenderManager().renderPosY).toFloat()
+                val z = (RenderUtil.interpolate(en.posZ, en.prevPosZ, mc.timer.renderPartialTicks) - mc.getRenderManager().renderPosZ).toFloat()
 
-                nametags.add(Pair(en, RenderUtil.project3d(x.toFloat(), y.toFloat() + en.height + 0.5F, z.toFloat())))
+                nametags.add(Pair(en, RenderUtil.project3d(x, y + en.height + 0.5F, z, VibrantClient.factory.newScaledResolution().getScaleFactor())))
             }
         }
     }
