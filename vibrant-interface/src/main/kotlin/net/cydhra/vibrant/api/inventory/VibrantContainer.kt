4e8c56681ff79a3dee3913b7560dce53bfe60872
Cydhra <ubezl@student.kit.edu>
2018-04-03 00:15:40 +0200

    hooked Container.clickSlot

--- vibrant-interface/src/main/kotlin/net/cydhra/vibrant/api/inventory/VibrantContainer.kt
+++ vibrant-interface/src/main/kotlin/net/cydhra/vibrant/api/inventory/VibrantContainer.kt
@@ -1,8 +1,14 @@
 package net.cydhra.vibrant.api.inventory
 
+import net.cydhra.vibrant.api.entity.VibrantPlayer
+import net.cydhra.vibrant.api.item.VibrantItemStack
+import net.cydhra.vibrant.api.network.ClickType
+
 interface VibrantContainer {
 
     val windowId: Int
 
     fun nextTransactionID(inventory: VibrantPlayerInventory): Short
+
+    fun clickSlot(slotId: Int, clickedButton: Int, mode: ClickType, player: VibrantPlayer): VibrantItemStack
 }
