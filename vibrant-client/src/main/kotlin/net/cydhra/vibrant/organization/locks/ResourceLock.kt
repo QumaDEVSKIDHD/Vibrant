0350124eaafdfe694d33fd9e195dc61bf617fe01
Cydhra <ubezl@student.kit.edu>
2018-04-03 19:49:36 +0200

    resource lock base class

---
+++ vibrant-client/src/main/kotlin/net/cydhra/vibrant/organization/locks/ResourceLock.kt
@@ -0,0 +1,9 @@
+package net.cydhra.vibrant.organization.locks
+
+import net.cydhra.vibrant.modulesystem.Module
+import net.cydhra.vibrant.organization.GameResource
+import net.cydhra.vibrant.organization.resources.GameResourceState
+
+class ResourceLock<S : GameResourceState>(val module: Module, resource: GameResource<S>, state: S) {
+
+}
