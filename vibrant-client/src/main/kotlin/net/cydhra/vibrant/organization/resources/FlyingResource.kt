bbcfa6184dc0eacf6dd45a65be5636d001536069
Cydhra <ubezl@student.kit.edu>
2018-04-09 23:34:09 +0200

    constructed use case for GameResourceState in FlyingResource

--- vibrant-client/src/main/kotlin/net/cydhra/vibrant/organization/resources/FlyingResource.kt
+++ vibrant-client/src/main/kotlin/net/cydhra/vibrant/organization/resources/FlyingResource.kt
@@ -1,28 +1,17 @@
 package net.cydhra.vibrant.organization.resources
 
-import net.cydhra.vibrant.VibrantClient
 import net.cydhra.vibrant.organization.GameResource
+import net.cydhra.vibrant.organization.channel.ResourceChannel
 
 /**
  *
  */
 object FlyingResource : GameResource<FlyingResource.FlyingResourceState>() {
 
-    override fun register(): MutableMap<GameResource<*>, in IResourceChannel<*>>.() -> Unit = {
-        this[FlyingResource] = ChannelBuilder.newBuilder(
-                {
-                    FlyingResourceState(VibrantClient.minecraft.thePlayer?.isFlying ?: false,
-                            VibrantClient.minecraft.thePlayer?.isAllowedFlying ?: false)
-                },
-                { side, state ->
-                    if (side == ResourceChannel.Side.CLIENT) {
-                        VibrantClient.minecraft.thePlayer?.isAllowedFlying = state.isAllowedFlying
-                        VibrantClient.minecraft.thePlayer?.isFlying = state.isFlying
-                    }
-                })
-                .convergent()
-                .create()
+    override fun register():
+            MutableMap<GameResource<*>, in ResourceChannel<*, FlyingResource.FlyingResourceState>>.() -> Unit = {
+        TODO()
     }
 
-    class FlyingResourceState(val isFlying: Boolean, val isAllowedFlying: Boolean) : GameResourceState()
+    class FlyingResourceState(val isFlying: Partial<Boolean>, val isAllowedFlying: Partial<Boolean>) : GameResourceState()
 }
