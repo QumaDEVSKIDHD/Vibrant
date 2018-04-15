7df4f42a0c826c5558839dd55a65c148a0ebfb92
Cydhra <ubezl@student.kit.edu>
2018-04-15 23:39:21 +0200

    implemented fly in new resource system

--- vibrant-client/src/main/kotlin/net/cydhra/vibrant/modules/movement/FlyModule.kt
+++ vibrant-client/src/main/kotlin/net/cydhra/vibrant/modules/movement/FlyModule.kt
@@ -1,7 +1,13 @@
 package net.cydhra.vibrant.modules.movement
 
+import net.cydhra.vibrant.VibrantClient
 import net.cydhra.vibrant.modulesystem.DefaultCategories
 import net.cydhra.vibrant.modulesystem.Module
+import net.cydhra.vibrant.organization.GameResourceManager
+import net.cydhra.vibrant.organization.locks.ResourceLock
+import net.cydhra.vibrant.organization.locks.Side
+import net.cydhra.vibrant.organization.priorities.ResourceRequestPriority
+import net.cydhra.vibrant.organization.resources.FlyingResource
 import org.lwjgl.input.Keyboard
 
 /**
@@ -9,21 +15,21 @@ import org.lwjgl.input.Keyboard
  */
 class FlyModule : Module("Fly", DefaultCategories.MOVEMENT, Keyboard.KEY_F) {
 
+    override fun onEnable() {
+        GameResourceManager.lockResource(
+                ResourceLock(
+                        this,
+                        FlyingResource,
+                        { !VibrantClient.minecraft.thePlayer!!.onGround },
+                        { ResourceRequestPriority.movement },
+                        { FlyingResource.FlyingResourceState(isFlying = true, isAllowedFlying = true) },
+                        Side.CLIENT
+                )
+        )
+    }
+
     override fun onDisable() {
         // this should not be done, but it is the simplest way
         mc.thePlayer?.isFlying = false
-    }
-
-    fun doRequestResources() {
-//        if (VibrantClient.minecraft.thePlayer != null) {
-//            if (!VibrantClient.minecraft.thePlayer!!.onGround) {
-//                GameResourceManager.requestGameResource(
-//                        FlyingResource,
-//                        FlyingResource.FlyingResourceState(isFlying = true, isAllowedFlying = true),
-//                        ResourceRequestPriority.movement,
-//                        ResourceChannel.Side.CLIENT
-//                )
-//            }
-//        }
     }
 }
