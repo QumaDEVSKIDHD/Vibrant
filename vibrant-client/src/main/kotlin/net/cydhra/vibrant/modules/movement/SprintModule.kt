fdce7bb22bb5e6a1894328e4bce352fdbb775ee4
Cydhra <ubezl@student.kit.edu>
2018-04-15 23:43:01 +0200

    implemented sprint in new resource system

--- vibrant-client/src/main/kotlin/net/cydhra/vibrant/modules/movement/SprintModule.kt
+++ vibrant-client/src/main/kotlin/net/cydhra/vibrant/modules/movement/SprintModule.kt
@@ -2,6 +2,11 @@ package net.cydhra.vibrant.modules.movement
 
 import net.cydhra.vibrant.modulesystem.DefaultCategories
 import net.cydhra.vibrant.modulesystem.Module
+import net.cydhra.vibrant.organization.GameResourceManager
+import net.cydhra.vibrant.organization.locks.ResourceLock
+import net.cydhra.vibrant.organization.locks.Side
+import net.cydhra.vibrant.organization.priorities.ResourceRequestPriority
+import net.cydhra.vibrant.organization.resources.SprintingResource
 import org.lwjgl.input.Keyboard
 
 /**
@@ -9,12 +14,16 @@ import org.lwjgl.input.Keyboard
  */
 class SprintModule : Module("Sprint", DefaultCategories.MOVEMENT, Keyboard.KEY_G) {
 
-    fun doRequestResources() {
-//        if (mc.thePlayer?.isCollidedHorizontally == false && mc.thePlayer!!.movementInput.moveForward > 0 && mc.thePlayer!!.onGround) {
-//            GameResourceManager.requestGameResource(
-//                    SprintingResource,
-//                    SprintingResource.SprintResourceState(isSprinting = true),
-//                    ResourceRequestPriority.speed) // a speed hack might declare it as movement to override this module
-//        }
+    override fun onEnable() {
+        GameResourceManager.lockResource(
+                ResourceLock(
+                        this,
+                        SprintingResource,
+                        { mc.thePlayer?.isCollidedHorizontally == false && mc.thePlayer!!.movementInput.moveForward > 0 && mc.thePlayer!!.onGround },
+                        { ResourceRequestPriority.speed },
+                        { SprintingResource.SprintResourceState(isSprinting = true) },
+                        Side.CLIENT
+                )
+        )
     }
 }
