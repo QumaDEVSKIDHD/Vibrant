8be02a886ffde334cffe3163ac1362d3bcc77dcb
Cydhra <ubezl@student.kit.edu>
2018-04-16 12:00:22 +0200

    ResourceChannelDecorator is now abstract as it may not be instantiated

--- vibrant-client/src/main/kotlin/net/cydhra/vibrant/organization/channel/ResourceChannelDecorator.kt
+++ vibrant-client/src/main/kotlin/net/cydhra/vibrant/organization/channel/ResourceChannelDecorator.kt
@@ -3,5 +3,5 @@ package net.cydhra.vibrant.organization.channel
 import net.cydhra.vibrant.organization.GameResource
 import net.cydhra.vibrant.organization.resources.GameResourceState
 
-open class ResourceChannelDecorator<G : GameResource<S>, S : GameResourceState>(protected val innerChannel: ResourceChannel<G, S>)
+abstract class ResourceChannelDecorator<G : GameResource<S>, S : GameResourceState>(protected val innerChannel: ResourceChannel<G, S>)
     : ResourceChannel<G, S> by innerChannel
